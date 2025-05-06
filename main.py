fa70255bdc9f6a6e9acde27082bae8ffc19b6eff
Adam Hines <adam.hines@qut.edu.au>
2025-05-06 15:05:32 +1000

    sequence length, fix pixi python and numpy versions

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ def parse_network():
                             help="Number of places to use for training and/or inferencing")
     parser.add_argument('--query_places', type=int, default=100,
                             help="Number of places to use for training and/or inferencing")
-    parser.add_argument('--sequence_length', type=int, default=10,
+    parser.add_argument('--sequence_length', type=int, default=3,
                         help="Length of the sequence matcher")
     parser.add_argument('--feature_multiplier', type=float, default=2.0,
                         help="Size multiplier for the feature/hidden layer")
